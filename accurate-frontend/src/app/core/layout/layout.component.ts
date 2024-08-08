8e1f9430637ca1fb7dea9123a0700def046cf220
keyurjivani1149 <keyurpatel1149@gmail.com>
2024-08-09 01:00:03 +0530

    card add in main page

--- accurate-frontend/src/app/core/layout/layout.component.ts
+++ accurate-frontend/src/app/core/layout/layout.component.ts
@@ -3,11 +3,12 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarComponent,HeaderComponent,FooterComponent,RouterOutlet],
+  imports: [SidebarComponent,HeaderComponent,FooterComponent,MatIconModule,RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
